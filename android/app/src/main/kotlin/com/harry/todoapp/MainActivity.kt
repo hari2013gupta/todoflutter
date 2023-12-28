3bed412d0d86fee804de3ea19aa19b89d741a09b
hari2013gupta <hk.mca08@gmail.com>
2023-12-28 16:26:03 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/harry/todoapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.harry.todoapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/data/apiClient/api_client.dart
@@ -0,0 +1,3 @@
+import 'package:harry_s_todo_app/core/app_export.dart';
+
+class ApiClient extends GetConnect {}
